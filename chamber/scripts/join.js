640b8624126c0190c2b919c4be54db759b99d462
jessicaseniw <jseniwborges@byupathway.edu>
2026-05-30 11:53:55 -0300

    Add membership cards and modal dialogs to join page

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,3 +1,20 @@
 const timestampField = document.querySelector("#timestamp");
 
 timestampField.value = new Date().toISOString();
+
+const links = document.querySelectorAll(".membership-card a");
+
+links.forEach(link => {
+
+    link.addEventListener("click", (event) => {
+
+        event.preventDefault();
+
+        const modalId =
+            link.getAttribute("href").replace("#", "");
+
+        document
+            .getElementById(modalId)
+            .showModal();
+    });
+});
